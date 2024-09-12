b173702ded17b02329bb46d5398766f168ff29f0
Erik Vank <noodly1234@outlook.com>
2024-09-12 08:08:27 -0400

    chore: add transition effects

--- web-ui/src/App.tsx
+++ web-ui/src/App.tsx
@@ -7,18 +7,27 @@ const App: React.FC = () => {
       {/* Navigation bar */}
       <header className="absolute top-0 right-0 p-8 text-white justify-center items-center">
         <nav className="flex space-x-8 justify-center items-center">
-          <a href="#about" className="hover:text-blue-500">
+          <a
+            href="#about"
+            className="hover:text-blue-500 transition duration-300"
+          >
             <span className="text-blue-500">[0]</span> About
           </a>
-          <a href="#experience" className="hover:text-blue-500">
+          <a
+            href="#experience"
+            className="hover:text-blue-500 transition duration-300"
+          >
             <span className="text-blue-500">[1]</span> Experience
           </a>
-          <a href="#contact" className="hover:text-blue-500">
+          <a
+            href="#contact"
+            className="hover:text-blue-500 transition duration-300"
+          >
             <span className="text-blue-500">[2]</span> Contact
           </a>
           <a
             href="/resume"
-            className="border border-blue-500 px-4 py-2 rounded hover:bg-blue-500 hover:text-black"
+            className="border border-blue-500 px-4 py-2 rounded hover:bg-blue-500 hover:text-black transition duration-300"
           >
             Resume
           </a>
@@ -29,7 +38,9 @@ const App: React.FC = () => {
       <main className="flex-grow flex flex-col items-start justify-center text-white px-8 lg:px-24">
         {/* Typing animation */}
         <div className="text-left">
-          <p className="text-blue-500 text-lg mb-2">Hi, my name is</p>
+          <p className="text-blue-500 text-lg mb-2 animate-pulse">
+            Hi, my name is
+          </p>
           <TypeAnimation
             className="font-bold text-6xl lg:text-8xl"
             sequence={["Erik Vank", 1000]}
@@ -50,7 +61,7 @@ const App: React.FC = () => {
         <div className="mt-8">
           <a
             href="#contact"
-            className="text-blue-500 border border-blue-500 px-6 py-3 rounded hover:bg-blue-500 hover:text-black transition"
+            className="text-blue-500 border border-blue-500 px-6 py-3 rounded hover:bg-blue-500 hover:text-black transition duration-300"
           >
             Get In Touch
           </a>
@@ -59,17 +70,20 @@ const App: React.FC = () => {
 
       {/* Social Icons Sidebar */}
       <div className="fixed bottom-2 left-4 flex flex-col space-y-4 text-blue-500">
-        <a href="#" className="hover:text-white">
+        <a href="#" className="hover:text-white transition duration-300">
           LinkedIn
         </a>
-        <a href="#" className="hover:text-white">
+        <a href="#" className="hover:text-white transition duration-300">
           GitHub
         </a>
       </div>
 
       {/* Footer Email */}
       <div className="fixed bottom-2 right-4 text-blue-500">
-        <a href="mailto:erik_vank@brown.edu" className="hover:text-white">
+        <a
+          href="mailto:erik_vank@brown.edu"
+          className="hover:text-white transition duration-300"
+        >
           erik_vank@brown.edu
         </a>
       </div>
