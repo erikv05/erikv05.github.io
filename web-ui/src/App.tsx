106d60ce6f26eaa46caf85e154815489618568b9
Erik Vank <noodly1234@outlook.com>
2024-09-11 22:37:26 -0400

    chore: fix color scheme

--- web-ui/src/App.tsx
+++ web-ui/src/App.tsx
@@ -3,25 +3,25 @@ import { TypeAnimation } from "react-type-animation";
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen bg-[#0a192f] flex flex-col">
+    <div className="min-h-screen bg-gray-900 flex flex-col">
       {/* Navigation bar */}
       <header className="absolute top-0 right-0 p-8 text-white">
         <nav className="flex space-x-8">
-          <a href="#about" className="hover:text-teal-400">
+          <a href="#about" className="hover:text-blue-500">
             About
           </a>
-          <a href="#experience" className="hover:text-teal-400">
+          <a href="#experience" className="hover:text-blue-500">
             Experience
           </a>
-          <a href="#work" className="hover:text-teal-400">
+          <a href="#work" className="hover:text-blue-500">
             Work
           </a>
-          <a href="#contact" className="hover:text-teal-400">
+          <a href="#contact" className="hover:text-blue-500">
             Contact
           </a>
           <a
             href="/resume"
-            className="border border-teal-400 px-4 py-2 rounded hover:bg-teal-400 hover:text-black"
+            className="border border-blue-500 px-4 py-2 rounded hover:bg-blue-500 hover:text-black"
           >
             Resume
           </a>
@@ -32,7 +32,7 @@ const App: React.FC = () => {
       <main className="flex-grow flex flex-col items-start justify-center text-white px-8 lg:px-24">
         {/* Typing animation */}
         <div className="text-left">
-          <p className="text-teal-400 text-lg mb-2">Hi, my name is</p>
+          <p className="text-blue-500 text-lg mb-2">Hi, my name is</p>
           <TypeAnimation
             className="font-bold text-6xl lg:text-8xl"
             sequence={["", 1000, "Erik Vank", 1000]}
@@ -41,7 +41,7 @@ const App: React.FC = () => {
             style={{ display: "inline-block" }}
             repeat={0}
           />
-          <p className="text-4xl lg:text-6xl mt-4 text-gray-400">
+          <p className="text-4xl lg:text-6xl mt-4 text-gray-500">
             I'm passionate about engineering.
           </p>
           <p className="text-lg lg:text-xl mt-4 text-gray-500 max-w-lg">
@@ -54,7 +54,7 @@ const App: React.FC = () => {
         <div className="mt-8">
           <a
             href="#contact"
-            className="text-teal-400 border border-teal-400 px-6 py-3 rounded hover:bg-teal-400 hover:text-black transition"
+            className="text-blue-500 border border-blue-500 px-6 py-3 rounded hover:bg-blue-500 hover:text-black transition"
           >
             Get In Touch
           </a>
@@ -62,7 +62,7 @@ const App: React.FC = () => {
       </main>
 
       {/* Social Icons Sidebar */}
-      <div className="fixed bottom-0 left-4 flex flex-col space-y-4 text-teal-400">
+      <div className="fixed bottom-0 left-4 flex flex-col space-y-4 text-blue-500">
         <a href="#" className="hover:text-white">
           LinkedIn
         </a>
@@ -75,15 +75,15 @@ const App: React.FC = () => {
         <a href="#" className="hover:text-white">
           Twitter
         </a>
-        <div className="w-px h-24 bg-teal-400"></div>
+        <div className="w-px h-24 bg-blue-500"></div>
       </div>
 
       {/* Footer Email */}
-      <div className="fixed bottom-0 right-4 text-teal-400 transform rotate-90">
+      <div className="fixed bottom-0 right-4 text-blue-500 transform rotate-90">
         <a href="mailto:erik_vank@brown.edu" className="hover:text-white">
           erik_vank@brown.edu
         </a>
-        <div className="w-px h-24 bg-teal-400 mt-4"></div>
+        <div className="w-px h-24 bg-blue-500 mt-4"></div>
       </div>
     </div>
   );
