671871d0ac58d1d15bda8f96f263cc583feb8d6d
Erik Vank <noodly1234@outlook.com>
2024-09-12 08:02:13 -0400

    fix: clean up UI elements

--- web-ui/src/App.tsx
+++ web-ui/src/App.tsx
@@ -3,18 +3,18 @@ import { TypeAnimation } from "react-type-animation";
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-900 flex flex-col">
+    <div className="min-h-screen bg-gray-900 flex flex-col font-mono">
       {/* Navigation bar */}
       <header className="absolute top-0 right-0 p-8 text-white justify-center items-center">
-        <nav className="flex space-x-8">
+        <nav className="flex space-x-8 justify-center items-center">
           <a href="#about" className="hover:text-blue-500">
-            About
+            <span className="text-blue-500">[0]</span> About
           </a>
           <a href="#experience" className="hover:text-blue-500">
-            Experience
+            <span className="text-blue-500">[1]</span> Experience
           </a>
           <a href="#contact" className="hover:text-blue-500">
-            Contact
+            <span className="text-blue-500">[2]</span> Contact
           </a>
           <a
             href="/resume"
@@ -32,18 +32,17 @@ const App: React.FC = () => {
           <p className="text-blue-500 text-lg mb-2">Hi, my name is</p>
           <TypeAnimation
             className="font-bold text-6xl lg:text-8xl"
-            sequence={["", 1000, "Erik Vank", 1000]}
-            wrapper="span"
-            speed={50}
-            style={{ display: "inline-block" }}
+            sequence={["Erik Vank", 1000]}
+            speed={10}
             repeat={0}
+            wrapper={"div"}
           />
-          <p className="text-4xl lg:text-6xl mt-4 text-gray-500">
-            I'm passionate about engineering.
+          <p className="text-4xl lg:text-6xl mt-4 text-gray-400">
+            I'm passionate about software engineering.
           </p>
-          <p className="text-lg lg:text-xl mt-4 text-gray-500 max-w-lg">
-            I love building web applications, researching large language models,
-            and everything in between.
+          <p className="text-lg lg:text-xl mt-4 text-gray-400 max-w-lg">
+            I love building web applications, researching the applications of
+            natural language processing, and everything in between.
           </p>
         </div>
 
@@ -59,21 +58,17 @@ const App: React.FC = () => {
       </main>
 
       {/* Social Icons Sidebar */}
-      <div className="fixed bottom-0 left-4 flex flex-col space-y-4 text-blue-500">
+      <div className="fixed bottom-2 left-4 flex flex-col space-y-4 text-blue-500">
         <a href="#" className="hover:text-white">
           LinkedIn
         </a>
-        <a
-          href="#"
-          className="hover:text-white"
-          style={{ paddingBottom: "1rem" }}
-        >
+        <a href="#" className="hover:text-white">
           GitHub
         </a>
       </div>
 
       {/* Footer Email */}
-      <div className="fixed bottom-0 right-4 text-blue-500 transform rotate-90">
+      <div className="fixed bottom-2 right-4 text-blue-500">
         <a href="mailto:erik_vank@brown.edu" className="hover:text-white">
           erik_vank@brown.edu
         </a>
